246e01dfc94f28a4782ac8c0abcd7e4fce3e900b
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2023-11-08 12:39:08 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student{
+  public String firstName;
+
+  public Student(String fName){
+    firstName = fName;
+  }
+  
+  public String toString(){
+    return "Your first name is " + firstName
+  }
+}
