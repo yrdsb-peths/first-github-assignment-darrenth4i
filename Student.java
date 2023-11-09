5754c76a6d7b8173e782b7eea0b9429bbef451ea
darrenth4i <150176044+darrenth4i@users.noreply.github.com>
2023-11-09 12:38:05 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,11 +1,13 @@
 public class Student{
   public String firstName;
+  public int volunteerHours;
 
-  public Student(String fName){
+  public Student(String fName, int numVolunteerHours){
     firstName = fName;
+    volunteerHours = numVolunteerHours;
   }
   
   public String toString(){
-    return "Your first name is " + firstName
+    return "Your first name is " + firstName + " and you have " + volunteerHours + " volunteer hours";
   }
 }
